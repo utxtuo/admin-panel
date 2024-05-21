5788bc3ac3f1dfce70c0e896f2581ab5ae3399d0
utxtuo <149408835+utxtuo@users.noreply.github.com>
2024-05-21 20:19:39 +0300

    Add files via upload

---
+++ middlewares/cors.js
@@ -0,0 +1,23 @@
+function cors(req, res, next) {
+    const { origin } = req.headers;
+    
+    if (allowedCors.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', '*');
+    }
+    
+    next();
+}
+
+const allowedCors = [
+    'https://practicum.yandex.ru',
+    'https://students-projects.ru',
+    'http://localhost:3000'
+  ];
+
+  app.use(
+    cors,
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    mainRoute,
+    gamesRouter
+  )
